1ba130cfb1107a85513c87a4c01229871a09034e
Vassily Krasnov <mykrasnov@gmail.com>
2018-01-15 01:59:32 +0300

    Indent lines

--- src/App.js
+++ src/App.js
@@ -6,23 +6,20 @@ import Filter from './Filter';
 import Tickets from './Tickets';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div className="Content">
-            <Filter />
-            <Tickets />
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <div className="Content">
+                    <Filter />
+                    <Tickets />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
