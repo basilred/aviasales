0465f63882e9a7af6e409e49c5f2d3d9e1692e9c
Vassily Krasnov <mykrasnov@gmail.com>
2018-01-15 02:18:32 +0300

    Add logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './App__logo.svg';
 import './App.css';
 
 import Filter from './Filter';
@@ -9,9 +9,8 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
+                <header className="App__header">
+                    <img src={logo} className="App__logo" alt="logo" />
                 </header>
                 <div className="Content">
                     <Filter />
