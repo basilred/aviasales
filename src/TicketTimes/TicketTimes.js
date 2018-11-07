5bff1c36d534ddffadff6a4a5a4eb822b33a4b01
Vassily Krasnov <mykrasnov@gmail.com>
2018-11-07 18:39:32 +0300

    Rewrite TicketTimes component to functional

--- src/TicketTimes/TicketTimes.js
+++ src/TicketTimes/TicketTimes.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './TicketTimes.css';
 
 import TicketStops from '../TicketStops/TicketStops';
 
-class TicketTimes extends Component {
-  static addLeadingZero(processedTime) {
-    return (processedTime.length < 5) ? `0${processedTime}` : processedTime;
-  }
+const addLeadingZero = processedTime => ((processedTime.length < 5) ? `0${processedTime}` : processedTime);
 
-  render() {
-    return (
-      <div className="TicketTimes">
-        <span className="Ticket__departure-time">
-          {TicketTimes.addLeadingZero(this.props.departure)}
-        </span>
-        <TicketStops stops={this.props.stops} />
-        <span className="Ticket__arrival-time">
-          {TicketTimes.addLeadingZero(this.props.arrival)}
-        </span>
-      </div>
-    );
-  }
-}
+const TicketTimes = ({ departure, arrival, stops }) => (
+  <div className="TicketTimes">
+    <span className="Ticket__departure-time">
+      {addLeadingZero(departure)}
+    </span>
+    <TicketStops stops={stops} />
+    <span className="Ticket__arrival-time">
+      {addLeadingZero(arrival)}
+    </span>
+  </div>
+);
 
 TicketTimes.propTypes = {
   departure: PropTypes.string.isRequired,
